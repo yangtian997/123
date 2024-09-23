54a8f6d434492e89ef8ab7773317f6d7b24dafc6
yangtian997 <148509644+yangtian997@users.noreply.github.com>
2024-09-23 15:35:00 +0800

    Update Header.vue

--- components/layout/netdisk/Header.vue
+++ components/layout/netdisk/Header.vue
@@ -3,17 +3,6 @@ const colorMode = useColorMode()
 
 </script>
 <template>
-    <div
-        class="bg-white/70 backdrop-blur dark:bg-gray-800 py-4 fixed top-0 left-0 w-full border-b border-slate-150 z-50">
-        <div class="max-w-[1240px] mx-auto flex flex-row items-center justify-between px-[20px]">
-            <div>
-                <!-- <nuxt-link to="/blog" class="text-sm text-slate-600 font-bold dark:text-white">我的博客</nuxt-link> -->
-            </div>
-            
-
-                
-            </div>
-        </div>
-    </div>
+    
 </template>
 <style scoped></style>
